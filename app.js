a885feb97bd14e4a93e140a9d3a0a503a075ae55
Prachi Sahani <prachirsahani97@gmail.com>
2021-09-01 20:51:50 +0530

    added functionality and style

---
+++ app.js
@@ -0,0 +1,32 @@
+var txtInput = document.querySelector('#txt-input')
+var btnTranslate = document.querySelector('#btn-translate')
+var baseUrl = 'https://api.funtranslations.com/translate/braille/html.json';
+var output = document.querySelector('#output')
+
+btnTranslate.addEventListener('click',translator);
+function translator(){
+    output.innerHTML='';
+    btnTranslate.innerHTML = 'Translating...';
+    fetch(getTranslationUrl(txtInput.value))
+    .then((response) => response.json())
+    .then((json) => {
+        if(json.success){
+            json.contents.translated.forEach(element => {
+                output.innerHTML+=element
+            });
+           btnTranslate.innerHTML = 'Translate'            
+        }   
+        else{
+            throw json.error;
+        }    
+    })
+    .catch(errorHandler)
+}
+
+function errorHandler(error){
+    alert(error.message)
+}
+
+function getTranslationUrl(text){
+    return `${baseUrl}?text=${text}`;
+}
